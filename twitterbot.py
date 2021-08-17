f8c21c0e2845cb00da414025874e59beafc95419
Lucas Villela <23728459+lcsvillela@users.noreply.github.com>
2021-08-17 05:18:49 -0300

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from sys import argv
@@ -17,17 +18,24 @@ class TwitterBot():
                  text="Fui automatizado! Confira: www.github.io/publicando-tweet-com-python.html"):
         self.__username = username
         self.__password = password
+        self.__links = {'login': 'https://twitter.com/login',
+                        'home': 'https://twitter.com/home'}
         self.__location = {'username': 'session[username_or_email]',
                            'password': 'session[password]',
                            'button_login': '//form/div/div[3]',
-                           'button_tweet': "//*[@data-testid='tweetButtonInline']"}
+                           'button_tweet': "//*[@data-testid='tweetButtonInline']",
+                           'text_field': "//*[contains(text(), 'happening')]",
+                           'text_id': "//[@data-testid='tweetTextarea_0_label']"}
         self.__driver = webdriver.Firefox()
-        self.__text = text
+        self.__text = "aaaaaaa" #text
         self.__wait = WebDriverWait(self.__driver, 30)
         self.__actions = ActionChains(self.__driver)
 
-    def login_twitter(self):
-        self.__driver.get("https://twitter.com/login")
+    def set_text(self, text):
+        self.__text = text
+
+    def login(self):
+        self.__driver.get(self.__links["login"])
         self.__wait.until(EC.presence_of_element_located((By.NAME, self.__location['username'])))
         self.__wait.until(EC.presence_of_element_located((By.NAME, self.__location['password'])))
         field_user = self.__driver.find_element_by_name(self.__location['username'])
@@ -37,14 +45,27 @@ class TwitterBot():
         self.__driver.find_element_by_xpath('//form/div/div[3]').click()
 
     def make_tweet(self):
-        self.__actions = ActionChains(self.__driver)
-        self.__wait.until(EC.presence_of_element_located((By.XPATH, self.__location['button_tweet'])))
-        ID = self.get_field_text()
-        self.__wait.until(EC.element_to_be_clickable((By.ID, ID)))
-        field_text = self.__driver.find_element_by_id(ID)
-        self.__actions.move_to_element(field_text).click(field_text).send_keys(self.__text).perform()
-        self.__driver.find_element_by_xpath(self.__location['button_tweet']).click()
+        try:
+            self.__actions = ActionChains(self.__driver)
+            self.__wait.until(EC.presence_of_element_located((By.XPATH, self.__location['button_tweet'])))
+            ID = self.get_field_text()
+            self.__wait.until(EC.element_to_be_clickable((By.ID, ID)))
+            field_text = self.__driver.find_element_by_id(ID)
+            self.__actions.move_to_element(field_text).click(field_text).send_keys(self.__text).perform()
+            self.__driver.find_element_by_xpath(self.__location['button_tweet']).click()
+        except:
+            self.refresh()
+            self.make_tweet()
+
+    def refresh(self):
+        self.__driver.get(self.__links["home"])
 
     def get_field_text(self):
         code = self.__driver.page_source.split('"')
         return [x for x in code if 'placeholder-' in x][0]
+
+    def close(self):
+        self.__driver.close()
+
+
+
